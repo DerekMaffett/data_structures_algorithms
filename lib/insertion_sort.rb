1a5b079908812771b449e689d77990999daf6d10
Derek Maffett <derekgmaffett@gmail.com>
2014-10-14 01:08:24 -0700

    change to bang method since it modifies array

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -1,6 +1,6 @@
 # Patch for implementing custom sorting algorithms on arrays
 class Array
-  def insertion_sort
+  def insertion_sort!
     each_with_index do |hold, i|
       until hold >= self[i - 1] || i == 0
         self[i] = self[i - 1]
--- spec/insertion_sort_spec.rb
+++ spec/insertion_sort_spec.rb
@@ -4,21 +4,21 @@ require 'benchmark'
 
 describe Array do
   it 'should return a sorted array with insertion sort' do
-    [1, 32, 4, 7, 42, 16].insertion_sort.must_equal [1, 4, 7, 16, 32, 42]
+    [1, 32, 4, 7, 42, 16].insertion_sort!.must_equal [1, 4, 7, 16, 32, 42]
   end
   it 'should sort an inverse array' do
-    (1..10).to_a.reverse.insertion_sort.must_equal (1..10).to_a
+    (1..10).to_a.reverse.insertion_sort!.must_equal (1..10).to_a
   end
   it 'should return same array if array is sorted' do
-    (1..10).to_a.insertion_sort.must_equal (1..10).to_a
+    (1..10).to_a.insertion_sort!.must_equal (1..10).to_a
   end
   it 'should sort a shuffled array' do
-    (1..10).to_a.shuffle.insertion_sort.must_equal (1..10).to_a
+    (1..10).to_a.shuffle.insertion_sort!.must_equal (1..10).to_a
   end
   it 'is benchmarked' do
-    best = Benchmark.measure { (1..1000).to_a.insertion_sort }
-    random = Benchmark.measure { (1..1000).to_a.shuffle.insertion_sort }
-    worst = Benchmark.measure { 1000.downto(1).to_a.insertion_sort }
+    best = Benchmark.measure { (1..1000).to_a.insertion_sort! }
+    random = Benchmark.measure { (1..1000).to_a.shuffle.insertion_sort! }
+    worst = Benchmark.measure { 1000.downto(1).to_a.insertion_sort! }
 
     puts "Best case: #{best}"
     puts "Random case: #{random}"
