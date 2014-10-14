383d6973def715bdd488cdb2e6c53f379dc87180
Derek Maffett <derekgmaffett@gmail.com>
2014-10-14 00:59:00 -0700

    implement better solution for insertion sort

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -1,47 +1,12 @@
 # Patch for implementing custom sorting algorithms on arrays
 class Array
   def insertion_sort
-    each_with_index do |_hold, i|
-      until self[i] >= self[i - 1] || i == 0
-        swap(i)
+    each_with_index do |hold, i|
+      until hold >= self[i - 1] || i == 0
+        self[i] = self[i - 1]
         i -= 1
       end
+      self[i] = hold
     end
   end
-
-  def swap(i)
-    self[i - 1], self[i] = self[i], self[i - 1]
-  end
-
-  # My current attempt at a comparison insertion sort. Not currently functional.
-  # def insertion_sort
-  #   self.each_with_index do |hold, i|
-  #     k = 0
-  #     while i - k > 0 && hold <= self[i - k]
-  #       puts k
-  #       k += 1
-  #     end
-  #     self.delete_at(i)
-  #     self.insert(i - k, hold)
-  #   end
-  #   self
-  # end
 end
-
-require 'benchmark'
-
-best = Benchmark.measure do
-  (1..1000).to_a.insertion_sort
-end
-
-middle = Benchmark.measure do
-  (1..1000).to_a.shuffle.insertion_sort
-end
-
-worst = Benchmark.measure do
-  (1..1000).to_a.reverse.insertion_sort
-end
-
-puts "Best case: #{best}"
-puts "Middle case: #{middle}"
-puts "Worst case: #{worst}"
--- spec/insertion_sort_spec.rb
+++ spec/insertion_sort_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'insertion_sort'
+require 'benchmark'
 
 describe Array do
   it 'should return a sorted array with insertion sort' do
@@ -13,5 +14,14 @@ describe Array do
   end
   it 'should sort a shuffled array' do
     (1..10).to_a.shuffle.insertion_sort.must_equal (1..10).to_a
+  end
+  it 'is benchmarked' do
+    best = Benchmark.measure { (1..1000).to_a.insertion_sort }
+    random = Benchmark.measure { (1..1000).to_a.shuffle.insertion_sort }
+    worst = Benchmark.measure { 1000.downto(1).to_a.insertion_sort }
+
+    puts "Best case: #{best}"
+    puts "Random case: #{random}"
+    puts "Worst case: #{worst}"
   end
 end
