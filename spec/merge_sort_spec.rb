9d9446e92310e47dd48d01affb052b719cfe1b84
Derek Maffett <derekgmaffett@gmail.com>
2014-10-15 09:41:55 -0700

    fix failing specs that resulted from rubocop refactor

--- spec/merge_sort_spec.rb
+++ spec/merge_sort_spec.rb
@@ -7,13 +7,13 @@ describe Array do
     [1, 32, 4, 7, 42, 16].merge_sort.must_equal [1, 4, 7, 16, 32, 42]
   end
   it 'merge sort should sort an inverse array' do
-    (1..10).to_a.reverse.merge_sort.must_equal(1..10).to_a
+    (1..10).to_a.reverse.merge_sort.must_equal((1..10).to_a)
   end
   it 'merge sort should return same array if array is sorted' do
-    (1..10).to_a.merge_sort.must_equal(1..10).to_a
+    (1..10).to_a.merge_sort.must_equal((1..10).to_a)
   end
   it 'merge sort should sort a shuffled array' do
-    (1..10).to_a.shuffle.merge_sort.must_equal(1..10).to_a
+    (1..10).to_a.shuffle.merge_sort.must_equal((1..10).to_a)
   end
   it 'is benchmarked' do
     best = Benchmark.measure { (1..1000).to_a.merge_sort }
