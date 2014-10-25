c7a5d174e1a8fa74ce6eb42f08e9c5dfe0f6b736
Derek Maffett <derekgmaffett@gmail.com>
2014-10-25 15:36:46 -0700

    implement full hash table

--- lib/data_structures.rb
+++ lib/data_structures.rb
@@ -37,7 +37,7 @@ module Structures
     end
 
     def insert(val)
-      @head = Node.new(val, head)
+      @head = Node.new(val, @head)
       @size += 1
       self
     end
@@ -147,6 +147,111 @@ module Structures
 
     def last
       @tail.try(:value)
+    end
+  end
+
+  class HashKeyError < RuntimeError; end
+
+  class HashNode
+    def initialize(key, value, nexxt)
+      @key = key
+      @value = value
+      @nexxt = nexxt
+    end
+
+    attr_accessor :key, :value, :nexxt
+  end
+
+  class HashLinkedList
+      def initialize
+        @size = 0
+        @head = nil
+      end
+
+      attr_reader :size
+
+      def unshift(key, val)
+        @head = HashNode.new(key, val, @head)
+        @size += 1
+        self
+      end
+
+      def search(key)
+        current_node = @head
+        until current_node.nil?
+          return current_node if current_node.key == key
+          current_node = current_node.nexxt
+        end
+      end
+
+      def remove(key)
+        if @head.key == key
+          @head = @head.nexxt
+          @size -= 1
+          return nil
+        end
+        current_node = @head
+        while current_node.nexxt
+          if current_node.nexxt.try(:key) == key
+            current_node.nexxt = current_node.nexxt.nexxt
+            @size -= 1
+            return nil
+          end
+        end
+      end
+
+      def each
+        current_node = @head
+        until current_node.nil?
+          yield(current_node)
+          current_node = current_node.nexxt
+        end
+      end
+    end
+
+  class HashTable
+    def initialize(allocation)
+      @allocation = allocation
+      @size = 0
+      @buckets = Array.new(@allocation)
+    end
+
+    attr_reader :size
+
+    def hash(key)
+      key.chars.inject(0) { |accum, c| accum + c.ord }
+    end
+
+    def set(key, value)
+      fail HashKeyError unless key.is_a? String
+      index = hash(key) % @allocation
+      @buckets[index] ||= HashLinkedList.new
+      return nullify(key, index) if value.nil?
+      table_entry = @buckets[index].search(key)
+      if table_entry
+        table_entry.key = key
+        table_entry.value = value
+      else
+        @buckets[index].unshift(key, value)
+        @size += 1
+      end
+      value
+    end
+
+    def get(key)
+      fail HashKeyError unless key.is_a? String
+      index = hash(key) % @allocation
+      return nil unless @buckets[index]
+      @buckets[index].search(key).try(:value)
+    end
+
+    private
+
+    def nullify(key, index)
+      initial_size = @buckets[index].size
+      @buckets[index].remove(key)
+      @size += @buckets[index].size - initial_size
+      nil
     end
   end
 end
---
+++ spec/hash_table_spec.rb
@@ -0,0 +1,68 @@
+require 'spec_helper'
+require 'data_structures'
+
+describe Structures::HashTable do
+  before do
+    @empty_hash_table = Structures::HashTable.new(1000)
+    @hash_table = Structures::HashTable.new(1000)
+    @hash_table.set('Steve', 'Apple Computers')
+  end
+
+  describe '#hash' do
+    it 'gives an appropriate additive hash for a given key' do
+      @empty_hash_table.hash('ab').must_equal 195
+    end
+  end
+
+  describe 'setting and getting' do
+    it 'should give an error if a key is not a string for #set' do
+      -> { @empty_hash_table.set(89, 9234) }.must_raise Structures::HashKeyError
+    end
+    it 'should give an error if a key is not a string for #get' do
+      -> { @empty_hash_table.get(89) }.must_raise Structures::HashKeyError
+    end
+
+    it 'should successfully retrieve a set k/v pair' do
+      @hash_table.get('Steve').must_equal 'Apple Computers'
+    end
+
+    it 'should return nil when k/v pair is not found' do
+      @hash_table.get('Bill').must_be_nil
+    end
+
+    it 'should get appropriate values given collisions' do
+      @empty_hash_table.set('cc', 'Correct Value')
+      @empty_hash_table.set('bd', 'Collision Value')
+      @empty_hash_table.get('cc').must_equal 'Correct Value'
+      @empty_hash_table.get('bd').must_equal 'Collision Value'
+    end
+
+    it 'should reset a k/v pair' do
+      @hash_table.set('Steve', 'Microsoft')
+      @hash_table.get('Steve').must_equal 'Microsoft'
+    end
+  end
+
+  describe '#size' do
+    it 'empty hash table should have size of 0' do
+      @empty_hash_table.size.must_equal 0
+    end
+
+    it 'hash table with entries should have a correct size' do
+      @hash_table.size.must_equal 1
+      @hash_table.set('Bill', 'Microsoft')
+      @hash_table.size.must_equal 2
+    end
+
+    it 'reseting a k/v pair should not change the size' do
+      @hash_table.set('Steve', 'Microsoft')
+      @hash_table.size.must_equal 1
+    end
+
+    it 'setting an entry to nil removes it from the table' do
+      @hash_table.size.must_equal 1
+      @hash_table.set('Steve', nil)
+      @hash_table.size.must_equal 0
+    end
+  end
+end
